4c0aa798b67889fb9d718143d41ba404d3c61df0
Maxim Bilokur <bilmaxim@gmail.com>
2020-10-22 00:22:48 +0300

    Lesson 1 essencials homeworks

---
+++ Lesson_001_Essencials_Classes_Homework/Задание2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Задание2
+{
+    class Rectangle
+    {
+        private double side1, side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            if (side1 != 0 && side2 != 0)
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+            }
+            else
+            { 
+                Console.WriteLine("Внимание, одно из значений равно нулю."); 
+            }
+        }
+        private double AreaCalculator()
+        {
+            double area = side1 * side2;
+            return area;
+        }
+        private double PerimeterCalculator()
+        {
+            double perimeter = (side1 + side2) * 2;
+            return perimeter;
+        }
+
+        public double Area()
+        {
+            return AreaCalculator();
+        }
+        public double Perimeter()
+        {
+            return PerimeterCalculator();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину одной стороны прямоугоьника: ");
+            int side1User = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину другой стороны прямоугоьника: ");
+            int side2User = Int32.Parse(Console.ReadLine());
+
+            Rectangle userRec = new Rectangle(side1User, side2User);
+            double area = userRec.Area();
+            double perimeter = userRec.Perimeter();
+            Console.WriteLine("Площадь прямоугольника равна " + area);
+            Console.WriteLine("Периметр прямоугольника равен " + perimeter);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson_001_Essencials_Classes_Homework/Задание3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Задание3;
+
+namespace Задание3
+{
+    class Book
+    {
+        Title title;
+        Author author;
+        Content content;
+
+        public Book(Title title, Author author, Content content)
+        {
+            this.title = title;
+            this.author = author;
+            this.content = content;
+        }
+
+        public void Show()
+        {
+            this.title.Show();
+            this.author.Show();
+            this.content.Show();
+        }
+    }
+    class Title
+    {
+        public string title;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    class Author
+    {
+        public string author;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(author);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    class Content
+    {
+        public string content;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(content);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введита имя автора книги");
+        string titleInput = Console.ReadLine();
+        Console.WriteLine("Введите название книги");
+        string authorInput = Console.ReadLine();
+        Console.WriteLine("Про что книга?");
+        string contentInput = Console.ReadLine();
+        Console.Clear();
+
+        Title myTitle = new Title();
+        myTitle.title = titleInput;
+
+        Author myAuthor = new Author();
+        myAuthor.author = authorInput;
+
+        Content myContent = new Content();
+        myContent.content = contentInput;
+
+        Book myBook = new Book(myTitle, myAuthor, myContent);
+        myBook.Show();
+
+        Console.Read();
+        }
+    }
+}
